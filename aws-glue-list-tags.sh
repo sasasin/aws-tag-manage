8d54fbfbeca18b2ccc57d70a035542649d86ca1e
Shinnosuke Suzuki <sasasin@sasasin.net>
2023-03-15 21:58:26 +0900

    Update aws-glue-list-tags.sh

--- aws-glue-list-tags.sh
+++ aws-glue-list-tags.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -o pipefail
 
+AWS_ACCOUNT_ID=$(\
+	aws sts get-caller-identity \
+	| jq -r '.Account'\
+)
+
 aws glue get-databases \
 | jq -r '.[][].Name' \
 | xargs -t -L1 -IXXXX aws glue get-tags --resource-arn "arn:aws:glue:"$AWS_REGION":"$AWS_ACCOUNT_ID":database/"XXXX
